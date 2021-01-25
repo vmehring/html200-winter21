19d66fb2c321fd210c34c8870e159ad538ab56f7
Valerie Mehring <vmehring@gmail.com>
2021-01-24 16:56:33 -0800

    js driving age check

---
+++ lesson02/canDrive.js
@@ -0,0 +1,10 @@
+const drivingAge = 16;
+let fName = 'Alder';
+let lName = 'Mehring';
+let aldersAge = 16;
+
+if (aldersAge >= drivingAge) {
+  console.log(fName + ' ' + lName + ' is old enough to drive! God help us all.');
+} else {
+  console.log(fName + ' ' + lName + ' is not yet old enough to drive! We get to delay the inevitable a little bit longer.');
+}
