ffbfcfaab2dbfe4e61c08cd7ae13c7a36f402e8d
Valerie Mehring <vmehring@gmail.com>
2021-02-08 13:49:58 -0800

    added challenge assignment

--- lesson04/scripts/script.js
+++ lesson04/scripts/script.js
@@ -2,10 +2,25 @@ function getSquare() {
 
   let number = prompt('Gimme a number');
 
-  function square(root){
+  function square(root) {
     root = root * root;
     alert('Your square is ' + root);
   }
 
   square(number);
+  
+}
+
+function multiplyReply() {
+
+  let num1 = prompt('Gimme a number...');
+  let num2 = prompt('...and another one');
+
+  function multiplyTwo(x, y) {
+    result = x * y;
+    alert('Your result is ' + result)
+  }
+
+  multiplyTwo(num1, num2);
+
 }
